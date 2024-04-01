e78daae4f846ca797b5fd202bd32473a5b05efe3
freamwork97 <88388709+freamwork97@users.noreply.github.com>
2024-04-01 16:53:25 +0900

    [Bronze I] Title: Brownie Points I, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/4291. Brownie Points I/Brownie Points I.py
+++ 백준/Bronze/4291. Brownie Points I/Brownie Points I.py
@@ -1,3 +1,5 @@
+import sys
+
 while True:
     s = 0
     o = 0
@@ -7,23 +9,23 @@ while True:
     if b == 0:
         break
     for _ in range(b):
-        x, y = map(int, input().split())
+        x, y = map(int, sys.stdin.readline().split())
         xl.append(x)
         yl.append(y)
 
     for i, j in zip(xl, yl):
-        if (
-            i > xl[(len(xl) + 1) // 2 - 1] and j > yl[(len(yl) + 1) // 2 - 1]
-        ):  # x,y: 양수
+        if i > xl[(len(xl) + 1) // 2 - 1] and j > yl[(len(yl) + 1) // 2 - 1]:  # 1사분면
             s += 1
         elif (
             i < xl[(len(xl) + 1) // 2 - 1] and j > yl[(len(yl) + 1) // 2 - 1]
-        ):  # x:음수, y:양수
+        ):  # 2사분면
             o += 1
         elif (
             i < xl[(len(xl) + 1) // 2 - 1] and j < yl[(len(yl) + 1) // 2 - 1]
-        ):  # x,y: 음수
+        ):  # 3사분면
             s += 1
-        elif i > xl[(len(xl) + 1) // 2 - 1] and j < yl[(len(yl) + 1) // 2 - 1]:
+        elif (
+            i > xl[(len(xl) + 1) // 2 - 1] and j < yl[(len(yl) + 1) // 2 - 1]
+        ):  # 4사분면
             o += 1
     print(s, o)
