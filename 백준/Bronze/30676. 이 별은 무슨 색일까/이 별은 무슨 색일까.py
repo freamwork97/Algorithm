4d7ac6e80084fa48d0e99f0197370ff16a4f5986
freamwork97 <88388709+freamwork97@users.noreply.github.com>
2024-04-03 10:43:23 +0900

    [Bronze V] Title: 이 별은 무슨 색일까, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/30676. 이 별은 무슨 색일까/이 별은 무슨 색일까.py
@@ -0,0 +1,15 @@
+lam = int(input())
+if lam >= 620 and lam <= 780:
+    print("Red")
+elif lam >= 590 and lam < 620:
+    print("Orange")
+elif lam >= 570 and lam < 590:
+    print("Yellow")
+elif lam >= 495 and lam < 570:
+    print("Green")
+elif lam >= 450 and lam < 495:
+    print("Blue")
+elif lam >= 425 and lam < 450:
+    print("Indigo")
+elif lam >= 380 and lam < 425:
+    print("Violet")
