75332cc2723b6e6236281c34bae3e4996771ab19
freamwork97 <88388709+freamwork97@users.noreply.github.com>
2024-05-02 20:07:26 +0900

    [Bronze IV] Title: 주사위 세개, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -0,0 +1,13 @@
+n1, n2, n3 = map(int, input().split())
+
+if n1 == n2 == n3:
+    print(10000 + n1 * 1000)
+elif n1 == n2 or n1 == n3 or n2 == n3:
+    if n1 == n2:
+        print(1000 + n1 * 100)
+    elif n1 == n3:
+        print(1000 + n1 * 100)
+    if n2 == n3:
+        print(1000 + n2 * 100)
+else:
+    print(100 * max(n1, n2, n3))
