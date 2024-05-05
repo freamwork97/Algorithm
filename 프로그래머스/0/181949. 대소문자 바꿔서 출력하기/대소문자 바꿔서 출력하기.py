5863e730ca02786cf3d070409f699325c313dd8b
freamwork97 <88388709+freamwork97@users.noreply.github.com>
2024-05-05 20:52:16 +0900

    [level 0] Title: 대소문자 바꿔서 출력하기, Time: 15.67 ms, Memory: 7.38 MB -BaekjoonHub

--- 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.py
+++ 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.py
@@ -1,8 +1,13 @@
 str = input()
-result = ""
-for i in range(len(str)):
-    if str[i].isupper():
-        result += str[i].lower()
-    elif str[i].islower():
-        result += str[i].upper()
-print(result)
+# result = ""
+result2 = [
+    str[i].lower() if str[i].isupper() else str[i].upper() for i in range(len(str))
+]
+# for i in range(len(str)):
+#     result += str[i].lower() if str[i].isupper() else str[i].upper()
+# if str[i].isupper():
+#     result += str[i].lower()
+# elif str[i].islower():
+#     result += str[i].upper()
+for i in result2:
+    print(i, end="")
