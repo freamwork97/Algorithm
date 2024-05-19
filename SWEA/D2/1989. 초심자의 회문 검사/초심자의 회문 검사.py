a32edb9bce330984031b25efef27b485059e218c
freamwork97 <88388709+freamwork97@users.noreply.github.com>
2024-05-19 14:15:59 +0900

    [D2] Title: 초심자의 회문 검사, Time: 102 ms, Memory: 43,896 KB -BaekjoonHub

--- SWEA/D2/1989. 초심자의 회문 검사/초심자의 회문 검사.py
+++ SWEA/D2/1989. 초심자의 회문 검사/초심자의 회문 검사.py
@@ -4,7 +4,5 @@ for test_case in range(1, T + 1):
     s = input()
     s_list = [i for i in s]
     revers_s_list = [i for i in s[::-1]]
-    num = 0
-    if s_list == revers_s_list:
-        num += 1
+    num = 1 if s_list == revers_s_list else 0
     print(f"#{test_case} {num}")
