3e4974197cc5ba182cbf8658d71c0686642fa60c
freamwork97 <88388709+freamwork97@users.noreply.github.com>
2024-05-25 19:04:41 +0900

    [D2] Title: 시각 덧셈, Time: 106 ms, Memory: 44,984 KB -BaekjoonHub

--- SWEA/D2/1976. 시각 덧셈/시각 덧셈.py
+++ SWEA/D2/1976. 시각 덧셈/시각 덧셈.py
@@ -5,7 +5,6 @@ for test_case in range(1, T + 1):
     m = m1 + m2
     h = h1 + h2
 
-    # Adjust the hours and minutes if necessary
     if m >= 60:
         h += 1
         m %= 60
@@ -13,7 +12,6 @@ for test_case in range(1, T + 1):
     if h > 12:
         h %= 12
 
-    # It seems like we need to ensure the hour is not 0, if it is then we should set it to 12.
     if h == 0:
         h = 12
 
