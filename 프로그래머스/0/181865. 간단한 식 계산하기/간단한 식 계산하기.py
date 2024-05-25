83115fc72c5084a0ac31a926558e9c4d711dab9f
freamwork97 <88388709+freamwork97@users.noreply.github.com>
2024-05-25 14:37:48 +0900

    [level 0] Title: 간단한 식 계산하기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.py
@@ -0,0 +1,10 @@
+def solution(binomial):
+    answer = 0
+    a,op,b = map(str,binomial.split())
+    if op == '+':
+        answer = int(a)+int(b)
+    elif op == '-':
+        answer = int(a)-int(b)
+    else:
+        answer = int(a)*int(b)
+    return answer
