1c26bc4a67bd09ab0b70288ebf5e67e4aa842126
freamwork97 <88388709+freamwork97@users.noreply.github.com>
2024-07-21 20:54:45 +0900

    [D2] Title: 수도 요금 경쟁 D2 Attack, Time: 120 ms, Memory: 44,008 KB -BaekjoonHub

--- SWEA/D2/1284. 수도 요금 경쟁 D2 Attack/수도 요금 경쟁 D2 Attack.py
+++ SWEA/D2/1284. 수도 요금 경쟁 D2 Attack/수도 요금 경쟁 D2 Attack.py
@@ -4,11 +4,10 @@ for test_case in range(1, t + 1):
     p, q, r, s, w = map(int, input().split())
 
     a = p * w
-    b = 0
     if r > w:
-        b += q
+        b = q
     else:
         l = w - r
-        b += q + s * l
+        b = q + s * l
 
     print(f'#{test_case} {b if a > b else a}')
