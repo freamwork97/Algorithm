02f768e97419c8e10dd01e87cf2c0dbb55f1e30b
freamwork97 <88388709+freamwork97@users.noreply.github.com>
2024-08-03 20:09:56 +0900

    [Bronze I] Title: 최소공배수, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.py
@@ -0,0 +1,13 @@
+import sys
+
+n = int(sys.stdin.readline())
+for _ in range(n):
+    a, b = map(int, sys.stdin.readline().split())
+    c, d = max(a, b), min(a, b)
+    t = 1
+    while t > 0:
+        t = c % d
+        c, d = d, t
+    answer = int(a * b / c)
+
+    print(answer)
