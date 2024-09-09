38f7e47133931ec0ed754b439b4fb5291e6bfd7a
freamwork97 <88388709+freamwork97@users.noreply.github.com>
2024-09-10 08:38:06 +0900

    [Bronze II] Title: 1번부터 문제의 상태가…?, Time: 32 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Bronze/23027. 1번부터 문제의 상태가…？/1번부터 문제의 상태가…？.py
@@ -0,0 +1,9 @@
+s = input()
+if 'A' in s:
+    print(s.replace('B', 'A').replace('C', 'A').replace('D', 'A').replace('F', 'A'))
+elif 'B' in s:
+    print(s.replace('C', 'B').replace('D', 'B').replace('F', 'B'))
+elif 'C' in s:
+    print(s.replace('D', 'C').replace('F', 'C'))
+else:
+    print('A' * len(s))
